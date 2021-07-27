92981e2ac867372f7b89671aea98eec20e12fc54
RugvedPatil <72656927+RugvedPatil@users.noreply.github.com>
2021-07-27 19:30:08 +0530

    improved animation

--- js/sketch.js
+++ js/sketch.js
@@ -5,23 +5,14 @@ let max_d;
 
 function setup()
 {
-    const canvas = createCanvas(400,400, WEBGL);
+    const canvas = createCanvas(350,350, WEBGL);
     canvas.parent("wave_div");
     ma = atan(1/sqrt(2));
-    max_d = dist(0,0,200,200);
-}
-
-function getScrollPercent() {
-    var h = document.documentElement, 
-        b = document.body,
-        st = 'scrollTop',
-        sh = 'scrollHeight';
-    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+    max_d = dist(10,10,200,200);
 }
 
 function draw()
 {
-    const scrollPre = getScrollPercent();
     background(255,255,255);
     ortho(-400,400,-600,600,10,1000);
     ambientLight(255,255,255);
@@ -33,7 +24,7 @@ function draw()
     {
         for(let x=0; x < width; x += w)
         {
-            ambientMaterial(25 + scrollPre);
+            ambientMaterial(125);
             push(); 
             let d = dist(x,z,width/2,height/2);
             let offset = map(d,10,max_d, -PI, PI);
